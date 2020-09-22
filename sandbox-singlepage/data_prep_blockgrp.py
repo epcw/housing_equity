b0d7985972f201aecb4b64852834013911bef65b
Patrick W. Zimmeman <pzimmerman@epcw.org>
2020-09-21 21:37:09 -0700

    Initial breakdown to 2013 & 2018 for block groups done.  Still need to do deltas

--- sandbox-singlepage/data_prep_blockgrp.py
+++ sandbox-singlepage/data_prep_blockgrp.py
@@ -6,92 +6,155 @@ from sklearn.cluster import KMeans
 df_rent = pd.read_csv('data/king_blockgrp_rent.csv', dtype={"GEOID": str, "TRACT_NUM": str, "YEAR":str, "BLOCK_GRP":str}) #NOTE: pre-filtered in SQL for King County
 
 #filter for 2013
-df_rent = df_rent[(df_rent['YEAR'] == '2013')]
+df_rent = df_rent[(df_rent['YEAR'] == '2013') | (df_rent['YEAR'] == '2018')]
 
 #sort median_costs_df by census query, creating new column-sorted dfs instead of rows
-df = df_rent[(df_rent['CENSUS_QUERY'] == 'B25071_001E')]
-df = df.rename(columns = {'DATA' : 'RENT_AS_PCT_INCOME'})
-df = df[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR','RENT_AS_PCT_INCOME']]
-costs_df25 = df_rent[(df_rent['CENSUS_QUERY'] == 'B25057_001E')]
-costs_df25 = costs_df25.rename(columns = {'DATA' : 'RENT_25PCTILE'})
-costs_df25 = costs_df25[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR','RENT_25PCTILE']]
-costs_df50 = df_rent[(df_rent['CENSUS_QUERY'] == 'B25058_001E')]
-costs_df50 = costs_df50.rename(columns = {'DATA' : 'RENT_50PCTILE'})
-costs_df50 = costs_df50[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR','RENT_50PCTILE']]
-costs_df75 = df_rent[(df_rent['CENSUS_QUERY'] == 'B25059_001E')]
-costs_df75 = costs_df75.rename(columns = {'DATA' : 'RENT_75PCTILE'})
-costs_df75 = costs_df75[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR','RENT_75PCTILE']]
-df = df.merge(costs_df25, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR']).drop_duplicates()
-df = df.merge(costs_df50, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR']).drop_duplicates()
-df = df.merge(costs_df75, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR']).drop_duplicates()
+df13 = df_rent[(df_rent['YEAR'] == '2013')]
+dfrent_13 = df13[(df13['CENSUS_QUERY'] == 'B25071_001E')]
+dfrent_13 = dfrent_13.rename(columns = {'DATA' : 'RENT_AS_PCT_INCOME_2013'})
+dfrent_13 = dfrent_13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','RENT_AS_PCT_INCOME_2013']]
+costs_df25_13 = df13[(df13['CENSUS_QUERY'] == 'B25057_001E')]
+costs_df25_13 = costs_df25_13.rename(columns = {'DATA' : 'RENT_25PCTILE_2013'})
+costs_df25_13 = costs_df25_13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','RENT_25PCTILE_2013']]
+costs_df50_13 = df13[(df13['CENSUS_QUERY'] == 'B25058_001E')]
+costs_df50_13 = costs_df50_13.rename(columns = {'DATA' : 'RENT_50PCTILE_2013'})
+costs_df50_13 = costs_df50_13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','RENT_50PCTILE_2013']]
+costs_df75_13 = df13[(df13['CENSUS_QUERY'] == 'B25059_001E')]
+costs_df75_13 = costs_df75_13.rename(columns = {'DATA' : 'RENT_75PCTILE_2013'})
+costs_df75_13 = costs_df75_13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','RENT_75PCTILE_2013']]
+df13 = dfrent_13.merge(costs_df25_13, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP']).drop_duplicates()
+df13 = df13.merge(costs_df50_13, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP']).drop_duplicates()
+df13 = df13.merge(costs_df75_13, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP']).drop_duplicates()
+
+df18 = df_rent[(df_rent['YEAR'] == '2018')]
+dfrent_18 = df18[(df18['CENSUS_QUERY'] == 'B25071_001E')]
+dfrent_18 = dfrent_18.rename(columns = {'DATA' : 'RENT_AS_PCT_INCOME_2018'})
+dfrent_18 = dfrent_18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','RENT_AS_PCT_INCOME_2018']]
+costs_df25_18 = df18[(df18['CENSUS_QUERY'] == 'B25057_001E')]
+costs_df25_18 = costs_df25_18.rename(columns = {'DATA' : 'RENT_25PCTILE_2018'})
+costs_df25_18 = costs_df25_18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','RENT_25PCTILE_2018']]
+costs_df50_18 = df18[(df18['CENSUS_QUERY'] == 'B25058_001E')]
+costs_df50_18 = costs_df50_18.rename(columns = {'DATA' : 'RENT_50PCTILE_2018'})
+costs_df50_18 = costs_df50_18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','RENT_50PCTILE_2018']]
+costs_df75_18 = df18[(df18['CENSUS_QUERY'] == 'B25059_001E')]
+costs_df75_18 = costs_df75_18.rename(columns = {'DATA' : 'RENT_75PCTILE_2018'})
+costs_df75_18 = costs_df75_18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','RENT_75PCTILE_2018']]
+df18 = dfrent_18.merge(costs_df25_18, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP']).drop_duplicates()
+df18 = df18.merge(costs_df50_18, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP']).drop_duplicates()
+df18 = df18.merge(costs_df75_18, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP']).drop_duplicates()
+
+df = df13 = df13.merge(df18, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP']).drop_duplicates()
 
 #load in race data
 rdf = pd.read_csv('data/king_blockgrp_race.csv', dtype={"GEOID": str, "TRACT_NUM": str, "YEAR":str, "BLOCK_GRP":str}) #NOTE: pre-filtered in SQL for King County
 
 #filter for year 2013
-rdf = rdf[(rdf['YEAR'] == '2013')]
+rdf = rdf[(rdf['YEAR'] == '2013') | (rdf['YEAR'] == '2018') ]
 
 gdf = pd.read_csv('data/wa_king_census_block_groups_distances.csv',
                    dtype={"block_group_geoid_a": str,"block_group_geoid_b": str})
 
-totpop = rdf[(rdf['CENSUS_QUERY'] == 'B01001_001E')]
-totpop = totpop[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA','YEAR']]
-totpop = totpop.rename(columns = {'DATA' : 'TOT_POP_2013'})
-white = rdf[(rdf['CENSUS_QUERY'] == 'B03002_003E')]
-white = white[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA','YEAR']]
-white = white.rename(columns = {'DATA' : 'pop_white_nonhisp_only'})
-black = rdf[(rdf['CENSUS_QUERY'] == 'B02001_003E')]
-black = black[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA','YEAR']]
-black = black.rename(columns = {'DATA' : 'pop_black_only'})
-native = rdf[(rdf['CENSUS_QUERY'] == 'B02001_004E')]
-native = native[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA','YEAR']]
-native = native.rename(columns = {'DATA' : 'pop_native_only'})
-asian = rdf[(rdf['CENSUS_QUERY'] == 'B02001_005E')]
-asian = asian[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA','YEAR']]
-asian = asian.rename(columns = {'DATA' : 'pop_asian_only'})
-polynesian = rdf[(rdf['CENSUS_QUERY'] == 'B02001_006E')]
-polynesian = polynesian[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA','YEAR']]
-polynesian = polynesian.rename(columns = {'DATA' : 'pop_polynesian_only'})
-latino = rdf[(rdf['CENSUS_QUERY'] == 'B03002_012E')]
-latino = latino[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA','YEAR']]
-latino = latino.rename(columns = {'DATA' : 'pop_hispanic'})
-other = rdf[(rdf['CENSUS_QUERY'] == 'B02001_007E')]
-other = other[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA','YEAR']]
-other = other.rename(columns = {'DATA' : 'pop_other_only'})
-multiracial = rdf[(rdf['CENSUS_QUERY'] == 'B02001_008E')]
-multiracial = multiracial[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA','YEAR']]
-multiracial = multiracial.rename(columns = {'DATA' : 'pop_multiracial'})
-racial = totpop.merge(white, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'])
-racial = racial.merge(black, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'])
-racial = racial.merge(native, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'])
-racial = racial.merge(asian, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'])
-racial = racial.merge(polynesian, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'])
-racial = racial.merge(latino, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'])
-racial = racial.merge(other, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'])
-racial = racial.merge(multiracial, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR'])
-race_data = racial[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','YEAR','TOT_POP_2013','pop_white_nonhisp_only','pop_black_only','pop_native_only','pop_asian_only','pop_polynesian_only','pop_hispanic','pop_other_only','pop_multiracial']]
+rdf13 = rdf[(rdf['YEAR'] == '2013')]
+totpop_13 = rdf13[(rdf13['CENSUS_QUERY'] == 'B01001_001E')]
+totpop_13 = totpop_13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA','YEAR']]
+totpop_13 = totpop_13.rename(columns = {'DATA' : 'TOT_POP_2013'})
+white_13 = rdf13[(rdf13['CENSUS_QUERY'] == 'B03002_003E')]
+white_13 = white_13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+white_13 = white_13.rename(columns = {'DATA' : 'pop_white_nonhisp_only_2013'})
+black_13 = rdf13[(rdf13['CENSUS_QUERY'] == 'B02001_003E')]
+black_13 = black_13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+black_13 = black_13.rename(columns = {'DATA' : 'pop_black_only_2013'})
+native_13 = rdf13[(rdf13['CENSUS_QUERY'] == 'B02001_004E')]
+native_13 = native_13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+native_13 = native_13.rename(columns = {'DATA' : 'pop_native_only_2013'})
+asian_13 = rdf13[(rdf13['CENSUS_QUERY'] == 'B02001_005E')]
+asian_13 = asian_13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+asian_13 = asian_13.rename(columns = {'DATA' : 'pop_asian_only_2013'})
+polynesian_13 = rdf13[(rdf13['CENSUS_QUERY'] == 'B02001_006E')]
+polynesian_13 = polynesian_13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+polynesian_13 = polynesian_13.rename(columns = {'DATA' : 'pop_polynesian_only_2013'})
+latino_13 = rdf13[(rdf13['CENSUS_QUERY'] == 'B03002_012E')]
+latino_13 = latino_13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+latino_13 = latino_13.rename(columns = {'DATA' : 'pop_hispanic_2013'})
+other_13 = rdf13[(rdf13['CENSUS_QUERY'] == 'B02001_007E')]
+other_13 = other_13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+other_13 = other_13.rename(columns = {'DATA' : 'pop_other_only_2013'})
+multiracial_13 = rdf13[(rdf13['CENSUS_QUERY'] == 'B02001_008E')]
+multiracial_13 = multiracial_13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+multiracial_13 = multiracial_13.rename(columns = {'DATA' : 'pop_multiracial_2013'})
+racial13 = totpop_13.merge(white_13, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial13 = racial13.merge(black_13, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial13 = racial13.merge(native_13, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial13 = racial13.merge(asian_13, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial13 = racial13.merge(polynesian_13, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial13 = racial13.merge(latino_13, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial13 = racial13.merge(other_13, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial13 = racial13.merge(multiracial_13, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+race_data13 = racial13[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','TOT_POP_2013','pop_white_nonhisp_only_2013','pop_black_only_2013','pop_native_only_2013','pop_asian_only_2013','pop_polynesian_only_2013','pop_hispanic_2013','pop_other_only_2013','pop_multiracial_2013']]
 
-df = df.merge(race_data, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP', 'YEAR'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP', 'YEAR']).drop_duplicates()
-df['minority_pop'] = df['TOT_POP_2013'] - df['pop_white_nonhisp_only']
-df['minority_pop_pct'] = df['minority_pop'] / df['TOT_POP_2013']
+rdf18 = rdf[(rdf['YEAR'] == '2018')]
+totpop_18 = rdf18[(rdf18['CENSUS_QUERY'] == 'B01001_001E')]
+totpop_18 = totpop_18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA','YEAR']]
+totpop_18 = totpop_18.rename(columns = {'DATA' : 'TOT_POP_2018'})
+white_18 = rdf18[(rdf18['CENSUS_QUERY'] == 'B03002_003E')]
+white_18 = white_18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+white_18 = white_18.rename(columns = {'DATA' : 'pop_white_nonhisp_only_2018'})
+black_18 = rdf18[(rdf18['CENSUS_QUERY'] == 'B02001_003E')]
+black_18 = black_18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+black_18 = black_18.rename(columns = {'DATA' : 'pop_black_only_2018'})
+native_18 = rdf18[(rdf18['CENSUS_QUERY'] == 'B02001_004E')]
+native_18 = native_18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+native_18 = native_18.rename(columns = {'DATA' : 'pop_native_only_2018'})
+asian_18 = rdf18[(rdf18['CENSUS_QUERY'] == 'B02001_005E')]
+asian_18 = asian_18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+asian_18 = asian_18.rename(columns = {'DATA' : 'pop_asian_only_2018'})
+polynesian_18 = rdf18[(rdf18['CENSUS_QUERY'] == 'B02001_006E')]
+polynesian_18 = polynesian_18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+polynesian_18 = polynesian_18.rename(columns = {'DATA' : 'pop_polynesian_only_2018'})
+latino_18 = rdf18[(rdf18['CENSUS_QUERY'] == 'B03002_012E')]
+latino_18 = latino_18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+latino_18 = latino_18.rename(columns = {'DATA' : 'pop_hispanic_2018'})
+other_18 = rdf18[(rdf18['CENSUS_QUERY'] == 'B02001_007E')]
+other_18 = other_18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+other_18 = other_18.rename(columns = {'DATA' : 'pop_other_only_2018'})
+multiracial_18 = rdf18[(rdf18['CENSUS_QUERY'] == 'B02001_008E')]
+multiracial_18 = multiracial_18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','DATA']]
+multiracial_18 = multiracial_18.rename(columns = {'DATA' : 'pop_multiracial_2018'})
+racial18 = totpop_18.merge(white_18, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial18 = racial18.merge(black_18, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial18 = racial18.merge(native_18, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial18 = racial18.merge(asian_18, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial18 = racial18.merge(polynesian_18, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial18 = racial18.merge(latino_18, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial18 = racial18.merge(other_18, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+racial18 = racial18.merge(multiracial_18, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'])
+race_data18 = racial18[['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP','TOT_POP_2018','pop_white_nonhisp_only_2018','pop_black_only_2018','pop_native_only_2018','pop_asian_only_2018','pop_polynesian_only_2018','pop_hispanic_2018','pop_other_only_2018','pop_multiracial_2018']]
+race_data = race_data13.merge(race_data18, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP']).drop_duplicates()
+
+df = df.merge(race_data, how = 'inner', left_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP'], right_on = ['GEOID','COUNTY','TRACT_NUM','BLOCK_GRP']).drop_duplicates()
+df['minority_pop_2013'] = df['TOT_POP_2013'] - df['pop_white_nonhisp_only_2013']
+df['minority_pop_pct_2013'] = df['minority_pop_2013'] / df['TOT_POP_2013']
 
 gdf = gdf.merge(df[['GEOID']], how='left', left_on='block_group_geoid_a', right_on='GEOID')
 gdf = gdf.rename(columns={'block_group_geoid_a':'GEOID_a'})
 gdf = gdf.merge(df[['GEOID']], how='left', left_on='block_group_geoid_b', right_on='GEOID')
 gdf = gdf.rename(columns={'block_group_geoid_b':'GEOID_b'})
 
-#merge dataframes to combine the different datasets so that you can calculate it.
-minority10 = df[['GEOID','minority_pop_pct']]
-gdf = gdf.merge(minority10, how = 'inner', left_on = ['GEOID_a'], right_on = ['GEOID'])
-gdf = gdf.rename(columns = {'minority_pop_pct':'minority_pop_pct_2010_a'})
-gdf = gdf.merge(minority10, how = 'inner', left_on = ['GEOID_b'], right_on = ['GEOID'])
-gdf = gdf.rename(columns = {'minority_pop_pct':'minority_pop_pct_2010_b'})
+#TODO ADD ALL CHANGES HERE
 
-lower_quartile_cost10 = df[['GEOID','RENT_25PCTILE']]
-gdf = gdf.merge(lower_quartile_cost10, how = 'inner', left_on = ['GEOID_a'], right_on = ['GEOID'])
-gdf = gdf.rename(columns = {'RENT_25PCTILE':'lower_quartile_rent_a'})
-gdf = gdf.merge(lower_quartile_cost10, how = 'inner', left_on = ['GEOID_b'], right_on = ['GEOID'])
-gdf = gdf.rename(columns = {'RENT_25PCTILE':'lower_quartile_rent_b'})
+
+#merge dataframes to combine the different datasets so that you can calculate it.
+minority13 = df[['GEOID','minority_pop_pct_2013']]
+gdf = gdf.merge(minority13, how = 'inner', left_on = ['GEOID_a'], right_on = ['GEOID'])
+gdf = gdf.rename(columns = {'minority_pop_pct_2013':'minority_pop_pct_2013_a'})
+gdf = gdf.merge(minority13, how = 'inner', left_on = ['GEOID_b'], right_on = ['GEOID'])
+gdf = gdf.rename(columns = {'minority_pop_pct_2013':'minority_pop_pct_2013_b'})
+
+lower_quartile_cost13 = df[['GEOID','RENT_25PCTILE_2013']]
+gdf = gdf.merge(lower_quartile_cost13, how = 'inner', left_on = ['GEOID_a'], right_on = ['GEOID'])
+gdf = gdf.rename(columns = {'RENT_25PCTILE_2013':'lower_quartile_rent_2013_a'})
+gdf = gdf.merge(lower_quartile_cost13, how = 'inner', left_on = ['GEOID_b'], right_on = ['GEOID'])
+gdf = gdf.rename(columns = {'RENT_25PCTILE_2013':'lower_quartile_rent_2013_b'})
 
 #calculate diff between the two tracts (and take absolute value since sign is meaningless here)
 gdf['minority_pop_pct_delta'] = (gdf.minority_pop_pct_2010_a - gdf.minority_pop_pct_2010_b).abs()
