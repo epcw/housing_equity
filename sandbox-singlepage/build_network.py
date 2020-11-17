1718841d9e091984a542a2ac3447a0f3c40c160f
Patrick W. Zimmeman <pzimmerman@epcw.org>
2020-11-16 17:07:15 -0800

    Moved node_trace2018 and edge_trace2018 to objects rather than dicts to save memory.

--- sandbox-singlepage/build_network.py
+++ sandbox-singlepage/build_network.py
@@ -29,19 +29,26 @@ print('loading graph files')
 #for graph_name_name, graph in graphs_dict.items():
 #    exec(graph_name + '=graph')
 
-node_trace2018 = {}
-edge_trace2018 = {}
-
 df_combo = pd.read_csv(ROOTBEER + 'data/df_combo.csv', dtype={"GEOID": str,"TRACT_NUM": str,"YEAR":str})
 
 #from build_network import get_nodes, get_edges
-print('creating graph objects')
 for graph_name in graphs_dict:
     key = str(graph_name).lstrip('G2018_')
-    node_trace2018[key] = go.Scatter(
+
+
+colorsIndex = {'wallingford':'#ef553b','rainier_beach':'#636efa'}  #manually assign colors
+colors = df_combo['neighborhood'].map(colorsIndex)
+
+network_dir = os.path.join(ROOTBEER + 'data/network/')
+
+print('adding nodes and edges to graphs')
+for graph_name in graphs_dict:
+    key = str(graph_name).lstrip('G2018_')
+    print('creating graph objects for ' + graph_name)
+    node_trace2018 = go.Scatter(
         x=[],
         y=[],
-        mode='markers+text',  #make markers+text to show labels
+        mode='markers+text',  # make markers+text to show labels
         text=[],
         hoverinfo='text',
         customdata=df_combo['GEOID'],
@@ -62,44 +69,39 @@ for graph_name in graphs_dict:
         ),
         showlegend=True,
         marker_line_width=1
-        )
-    edge_trace2018[key] = go.Scatter(
+    )
+    edge_trace2018 = go.Scatter(
         x=[],
         y=[],
         line=dict(width=1, color='#c6c6c6'),
         hoverinfo='text',
         mode='lines'
-        )
-
-colorsIndex = {'wallingford':'#ef553b','rainier_beach':'#636efa'}  #manually assign colors
-colors = df_combo['neighborhood'].map(colorsIndex)
-
-network_dir = os.path.join(ROOTBEER + 'data/network/')
-
-print('adding nodes and edges to graphs')
-for graph_name in graphs_dict:
-    key = str(graph_name).lstrip('G2018_')
+    )
     print('building edges for ' + graph_name)
     for edge in graphs_dict[graph_name].edges():
         x0, y0 = graphs_dict[graph_name].nodes[edge[0]]['pos']
         x1, y1 = graphs_dict[graph_name].nodes[edge[1]]['pos']
-        edge_trace2018[key]['x'] += tuple([x0, x1, None])
-        edge_trace2018[key]['y'] += tuple([y0, y1, None])
+        edge_trace2018['x'] += tuple([x0, x1, None])
+        edge_trace2018['y'] += tuple([y0, y1, None])
 
     print('building nodes for ' + graph_name)
     for node in graphs_dict[graph_name].nodes():
         x, y = graphs_dict[graph_name].nodes[node]['pos']
         print(graph_name + ' ' + node + ' x')
-        node_trace2018[key]['x'] += tuple([x])
+        node_trace2018['x'] += tuple([x])
         print(graph_name + ' ' + node + ' y')
-        node_trace2018[key]['y'] += tuple([y])
+        node_trace2018['y'] += tuple([y])
         print(graph_name + ' ' + node + ' text')
-        node_trace2018[key].text = df_combo['neighborhood'] + '<br>' + df_combo["TRACT_NUM"]  # tract version
+        node_trace2018.text = df_combo['neighborhood'] + '<br>' + df_combo["TRACT_NUM"]  # tract version
         print(graph_name + ' ' + node + ' markers')
-        node_trace2018[key].marker.color = colors
-        node_trace2018[key].marker.size = (1.5 + df_combo.omega18) * 20
+        node_trace2018.marker.color = colors
+        node_trace2018.marker.size = (1.5 + df_combo.omega18) * 20
 
-    fig = go.Figure(data=[edge_trace2018[key],node_trace2018[key]],
+#    node_adjacencies = []
+#   for node, adjacencies in enumerate(graphs_dict[graph_name].items().adjacency()):
+#        node_adjacencies.append(len(adjacencies[1]))
+
+    fig = go.Figure(data=[edge_trace2018,node_trace2018],
                     layout=go.Layout(
                         title='',
                         titlefont=dict(size=16),
@@ -113,17 +115,14 @@ for graph_name in graphs_dict:
     with open(os.path.join(network_dir, 'network_{key}.json'.format(key=key)), 'w') as network_file:
         fig.write_json(network_file)
 
-    break
 
-print('calculating adjacencies')
-
-node_adjacencies = {}
-for graph_name in graphs_dict:
-    key = str(graph_name).lstrip('G2018_')
-    node_adjacencies[key] = []
-    for node, adjacencies in enumerate(graphs_dict[graph].adjacency()):
-        node_adjacencies[key].append(len(adjacencies[1]))
-
+#node_adjacencies = {}
+#for graph_name in graphs_dict:
+#    key = str(graph_name).lstrip('G2018_')
+#    node_adjacencies[key] = []
+#    for node, adjacencies in enumerate(graphs_dict[graph].items().adjacency()):
+#        node_adjacencies[key].append(len(adjacencies[1]))
+'''
 def get_nodes(subset='a1b1c1d1e1f1g1'):
     subsets = {}
     for node_trace in node_trace2018:
@@ -146,7 +145,7 @@ def get_edges(subset='a1b1c1d1e1f1g1'):
     else:
         raise ('ERROR - Unrecognized subset. Must be one of {}, bet received: {}'.format(subsets.keys(), subset))
 
-
+'''
 def get_maps(subset='one'):
     subsets = {
         'one': df_combo,
